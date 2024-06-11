a104c0455c7240ad410fbaba346e783277185aef
rashid4023 <mamunur@mds.com.bd>
2024-06-11 19:33:17 +0600

    Updates

---
+++ assets/index-D-rxjfuw.js
@@ -0,0 +1,1 @@
+const o={SIGNIN:"Sign In",SIGNUP:"Sign Up",DO_YOU_HAVE_AN_ACCOUNT:"Do you have account?",DONT_HAVE_ACCOUNT:"Don't you have account?",LOGOUT:"Logout",PROFILE:"Profile",CHAT_BOT:"Chatbot",SEARCH:"Search",CONVERSATIONS:"Conversations"};export{o as C};
